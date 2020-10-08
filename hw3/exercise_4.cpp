b25967c8c28356786c33877310b02f5866b5cab6
PlasmaNeon <zedek1ah@outlook.com>
2020-10-08 22:12:15 +0800

    Finish hw3.

--- hw3/exercise_4.cpp
+++ hw3/exercise_4.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     /********** Begin **********/
-    bool exercisel = true;
+    bool exercisel = false;
     if (exercisel){
     //第一关执行代码 完成 Interpolation.cpp与 Interpolation.h文件
    		string str;          //测试输入数据存放变量
@@ -86,7 +86,86 @@ int main()
 
     
     } else{
-   	//第二执行代码 完成 SimpleLinearRegression.cpp与 SimpleLinearRegression.h文件
+   	    //第二执行代码 完成 SimpleLinearRegression.cpp与 SimpleLinearRegression.h文件
+        string str; 
+        getline(cin, str);
+        size_t sz;
+        bool valid = true;
+        double x = stod(str, &sz);
+        if (sz != str.length()) {
+            valid = false;
+            cout << "error\nerror\nerror" << endl;
+            return 0;
+        }
+        map<double, double> input;
+        while (getline(cin, str)){
+            if (str.size() == 0) break;
+            if (valid){
+                size_t index;
+                double xx = stod(str, &index);
+                if (str[index] != ' ' || index >= str.length()){
+                    valid = false;
+                    break;
+                }
+                string sy = str.substr(index + 1);
+                double yy = stod(sy, &index);
+                if (index != sy.length())
+                    valid = false;
+                if (valid)
+                    input.insert({xx, yy});
+            }
+        }
+        if (!valid){
+            cout << "error\nerror\nerror" << endl;
+            return 0;
+        }
+        double x_sum = 0.0;
+        double y_sum = 0.0;
+        
+        for (auto& kv : input){
+            x_sum += kv.first;
+            y_sum += kv.second;
+        }
+        double x_bar = x_sum / input.size();
+        double y_bar = y_sum / input.size();
+
+        double lxx = 0.0;
+        double lyy = 0.0;
+        double lxy = 0.0;
+        for (auto& kv : input){
+            lxx = lxx + (kv.first - x_bar) * (kv.first - x_bar);
+            lyy = lyy + (kv.second - y_bar) * (kv.second - y_bar);
+            lxy = lxy + (kv.first - x_bar) * (kv.second - y_bar);
+        }
+        double r;
+        if (lxx * lyy == 0) cout << "error" <<endl;
+        else {
+            r = lxy/ (sqrt(lxx * lyy));
+            string sr = to_string(r);
+            sr.erase(sr.length() - 2);
+            cout << sr << endl;
+        }
+
+        if (lxx == 0 || fabs(r) <= 0.75) {
+            cout << "error\nerror" << endl;
+            return 0;
+        }
+
+        double b = lxy / lxx;
+        double a = y_bar - b * x_bar;
+        double ans = a + b * x;
+        string sa = to_string(a);
+        sa.erase(sa.length() - 2);
+        string sb = to_string(b);
+        sb.erase(sb.length() - 2);
+        cout << "y=" << sb << "*x";
+        if (a > 0) cout << '+' << sa << endl;
+        else if (a < 0) cout << sa << endl;
+        string sans = to_string(ans);
+        sans.erase(sans.length() - 2);
+        cout << sans << endl;
+
+
     }
     
 	/********** End **********/
